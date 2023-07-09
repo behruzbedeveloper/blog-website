e7f229e57c0c81d1f6cca94326b4eec09952bd5f
Dilmurodov Behruz <111862901+DBEHRUZ@users.noreply.github.com>
2023-07-09 16:50:26 +0500

    my blog web-sitet

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyAi8iPPZD0_E_DxcMYlxi7tFnEP6yL2bEU",
+    authDomain: "bloging-2a387.firebaseapp.com",
+    projectId: "bloging-2a387",
+    storageBucket: "bloging-2a387.appspot.com",
+    messagingSenderId: "933255250975",
+    appId: "1:933255250975:web:9921753466399794db012a"};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
